ce3bd5058b205642c2c75f9425a8809dc6f7a23d
ivan <ivan.serg0692@gmail.com>
2026-04-22 17:20:43 +0800

    task 3: the news-field config and the createby field

--- symfony/src/Controller/Admin/NewsCrudController.php
+++ symfony/src/Controller/Admin/NewsCrudController.php
@@ -3,7 +3,10 @@
 namespace App\Controller\Admin;
 
 use App\Entity\News;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Config\KeyValueStore;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\SlugField;
@@ -12,9 +15,26 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class NewsCrudController extends AbstractCrudController
 {
+    private const DEFAULT_HIDDEN_INDEX_COLUMNS = ['brief', 'description'];
+
     public static function getEntityFqcn(): string
     {
         return News::class;
+    }
+
+    public function configureCrud(Crud $crud): Crud
+    {
+        return $crud
+            ->overrideTemplate('crud/index', 'admin/news/index.html.twig');
+    }
+
+    public function configureResponseParameters(KeyValueStore $responseParameters): KeyValueStore
+    {
+        if (Crud::PAGE_INDEX === $responseParameters->get('pageName')) {
+            $responseParameters->set('default_hidden_columns', self::DEFAULT_HIDDEN_INDEX_COLUMNS);
+        }
+
+        return $responseParameters;
     }
 
     public function configureFields(string $pageName): iterable
@@ -26,6 +46,8 @@ class NewsCrudController extends AbstractCrudController
                 ->setTargetFieldName('name')
                 ->setUnlockConfirmationMessage('Edit slug manually?')
                 ->setHelp('Slug will be generated automatically'),
+            AssociationField::new('createdBy')
+                ->setFormTypeOption('choice_label', 'email')->setDisabled(true),
             TextEditorField::new('brief'),
             TextEditorField::new('description'),
             DateTimeField::new('createdAt', 'Created at')
