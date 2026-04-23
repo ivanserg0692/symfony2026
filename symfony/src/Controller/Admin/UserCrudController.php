056bae3d19c341f250bda5e78767703ff1d67180
ivan <ivan.serg0692@gmail.com>
2026-04-23 18:56:33 +0800

    task 3: added changing a password field

--- symfony/src/Controller/Admin/UserCrudController.php
+++ symfony/src/Controller/Admin/UserCrudController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
@@ -16,9 +17,16 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Filter\ArrayFilter;
 use EasyCorp\Bundle\EasyAdminBundle\Filter\EntityFilter;
 use EasyCorp\Bundle\EasyAdminBundle\Filter\TextFilter;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 final class UserCrudController extends AbstractCrudController
 {
+    public function __construct(
+        private readonly UserPasswordHasherInterface $passwordHasher,
+    ) {
+    }
+
     public static function getEntityFqcn(): string
     {
         return User::class;
@@ -42,7 +50,6 @@ final class UserCrudController extends AbstractCrudController
             ->add(ArrayFilter::new('roles'))
             ->add(EntityFilter::new('groups'));
     }
-
     public function configureActions(Actions $actions): Actions
     {
         $backToList = Action::new('backToList', 'Back to list', 'fa fa-arrow-left')
@@ -63,10 +70,45 @@ final class UserCrudController extends AbstractCrudController
 
         yield TextField::new('secondName');
 
+        yield TextField::new('plainPassword', 'New password')
+            ->onlyOnForms()
+            ->setFormType(PasswordType::class)
+            ->setRequired(Crud::PAGE_NEW === $pageName)
+            ->setHelp(Crud::PAGE_EDIT === $pageName ? 'Leave empty to keep the current password.' : null);
+
         yield ArrayField::new('roles');
 
         yield AssociationField::new('groups')
             ->setFormTypeOption('by_reference', false)
             ->setFormTypeOption('choice_label', 'name');
+    }
+
+    public function persistEntity(EntityManagerInterface $entityManager, $entityInstance): void
+    {
+        $this->hashPlainPassword($entityInstance);
+
+        parent::persistEntity($entityManager, $entityInstance);
+    }
+
+    public function updateEntity(EntityManagerInterface $entityManager, $entityInstance): void
+    {
+        $this->hashPlainPassword($entityInstance);
+
+        parent::updateEntity($entityManager, $entityInstance);
+    }
+
+    private function hashPlainPassword(object $entityInstance): void
+    {
+        if (!$entityInstance instanceof User) {
+            return;
+        }
+
+        $plainPassword = $entityInstance->getPlainPassword();
+        if ($plainPassword === null || $plainPassword === '') {
+            return;
+        }
+
+        $entityInstance->setPassword($this->passwordHasher->hashPassword($entityInstance, $plainPassword));
+        $entityInstance->setPlainPassword(null);
     }
 }
