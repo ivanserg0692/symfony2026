7fea93e2378e71eb375cc42efef5d8f668620a0b
ivan <ivan.serg0692@gmail.com>
2026-05-05 23:54:45 +0800

    task 5: add download action of the news export

--- symfony/src/Controller/Admin/NewsExportCrudController.php
+++ symfony/src/Controller/Admin/NewsExportCrudController.php
@@ -3,17 +3,31 @@
 namespace App\Controller\Admin;
 
 use App\Entity\NewsExport;
+use EasyCorp\Bundle\EasyAdminBundle\Attribute\AdminRoute;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IntegerField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use League\Flysystem\FilesystemException;
+use League\Flysystem\FilesystemOperator;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\StreamedResponse;
+use Symfony\Contracts\Translation\TranslatorInterface;
 
 class NewsExportCrudController extends AbstractCrudController
 {
+    public function __construct(
+        private readonly FilesystemOperator $newsExportStorage,
+    ) {
+    }
+
     public static function getEntityFqcn(): string
     {
         return NewsExport::class;
@@ -27,8 +41,63 @@ class NewsExportCrudController extends AbstractCrudController
 
     public function configureActions(Actions $actions): Actions
     {
+        $download = Action::new('download', 'admin.news_export.action.download', 'fas fa-download')
+            ->linkToCrudAction('download')
+            ->displayIf(fn (NewsExport $newsExport): bool => null !== $newsExport->getFilePath());
+
         return $actions
+            ->add(Crud::PAGE_INDEX, $download)
+            ->add(Crud::PAGE_DETAIL, $download)
             ->disable(Action::NEW, Action::EDIT, Action::DELETE, Action::BATCH_DELETE);
+    }
+
+    #[AdminRoute]
+    public function download(
+        AdminContext $context,
+        Request $request,
+        TranslatorInterface $translator,
+    ): Response {
+        $newsExport = $context->getEntity()->getInstance();
+
+        if (!$newsExport instanceof NewsExport) {
+            throw $this->createNotFoundException();
+        }
+
+        $filePath = $newsExport->getFilePath();
+
+        if (null === $filePath) {
+            $this->addFlash('warning', $translator->trans('admin.news_export.flash.file_not_ready'));
+
+            return $this->redirectToReferrer($request);
+        }
+
+        try {
+            $stream = $this->newsExportStorage->readStream($filePath);
+        } catch (FilesystemException) {
+            $this->addFlash('danger', $translator->trans('admin.news_export.flash.file_not_found'));
+
+            return $this->redirectToReferrer($request);
+        }
+
+        if (!\is_resource($stream)) {
+            $this->addFlash('danger', $translator->trans('admin.news_export.flash.file_not_found'));
+
+            return $this->redirectToReferrer($request);
+        }
+
+        $filename = sprintf('news-export-%d.csv', $newsExport->getId());
+
+        return new StreamedResponse(
+            static function () use ($stream): void {
+                fpassthru($stream);
+                fclose($stream);
+            },
+            Response::HTTP_OK,
+            [
+                'Content-Type' => 'text/csv; charset=UTF-8',
+                'Content-Disposition' => sprintf('attachment; filename="%s"', $filename),
+            ],
+        );
     }
 
     public function configureFields(string $pageName): iterable
@@ -42,5 +111,12 @@ class NewsExportCrudController extends AbstractCrudController
         yield DateTimeField::new('messengerBatch.createdAt', 'admin.news_export.field.created_at');
         yield DateTimeField::new('messengerBatch.startedAt', 'admin.news_export.field.started_at');
         yield DateTimeField::new('messengerBatch.finishedAt', 'admin.news_export.field.finished_at');
+    }
+
+    private function redirectToReferrer(Request $request): RedirectResponse
+    {
+        return $this->redirect($request->headers->get('referer') ?? $this->generateUrl('admin', [
+            '_locale' => $request->getLocale(),
+        ]));
     }
 }
