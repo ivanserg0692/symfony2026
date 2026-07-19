805d9e31aa1441a215a5036ab4ed3099b9938b47
ivan <ivan.serg0692@gmail.com>
2026-07-19 20:38:52 +0800

    task 7: generate php GRPC files, add the proto files

---
+++ cart-service/src/Grpc/Generated/Grpc/Catalog/V1/InventoryServiceClient.php
@@ -0,0 +1,33 @@
+<?php
+// GENERATED CODE -- DO NOT EDIT!
+
+namespace Grpc\Catalog\V1;
+
+/**
+ */
+class InventoryServiceClient extends \Grpc\BaseStub {
+
+    /**
+     * @param string $hostname hostname
+     * @param array $opts channel options
+     * @param \Grpc\Channel $channel (optional) re-use channel object
+     */
+    public function __construct($hostname, $opts, $channel = null) {
+        parent::__construct($hostname, $opts, $channel);
+    }
+
+    /**
+     * @param \Grpc\Catalog\V1\CheckStockRequest $argument input argument
+     * @param array $metadata metadata
+     * @param array $options call options
+     * @return \Grpc\UnaryCall<\Grpc\Catalog\V1\CheckStockResponse>
+     */
+    public function CheckStock(\Grpc\Catalog\V1\CheckStockRequest $argument,
+      $metadata = [], $options = []) {
+        return $this->_simpleRequest('/catalog.v1.InventoryService/CheckStock',
+        $argument,
+        ['\Grpc\Catalog\V1\CheckStockResponse', 'decode'],
+        $metadata, $options);
+    }
+
+}
