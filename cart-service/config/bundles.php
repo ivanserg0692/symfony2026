1668acca5ead7c5a245d5fffe790a41f700de8b6
ivan <ivan.serg0692@gmail.com>
2026-07-19 19:44:45 +0800

    task 7.1: install the cart debugger

--- cart-service/config/bundles.php
+++ cart-service/config/bundles.php
@@ -7,4 +7,7 @@ return [
     Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
