ef7112e9e2a3822bafa747e8a3828738d15e2059
ivan <ivan.serg0692@gmail.com>
2026-07-23 23:54:22 +0800

    task 7.2: install stof/doctrine-extensions-bundle

--- cart-service/config/bundles.php
+++ cart-service/config/bundles.php
@@ -10,4 +10,5 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
 ];
