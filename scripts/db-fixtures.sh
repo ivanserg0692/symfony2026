ffd5be6fdd7c925025f082ddbdcde09a4d0089c2
ivan <ivan.serg0692@gmail.com>
2026-08-24 18:24:37 +0800

    task 9: fix the fixture installation

--- scripts/db-fixtures.sh
+++ scripts/db-fixtures.sh
@@ -5,4 +5,4 @@ set -euo pipefail
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "${SCRIPT_DIR}/db-common.sh"
 
-CONSOLE_APP_ENV=dev run_for_database_services "doctrine:fixtures:load --no-interaction --no-debug" -d memory_limit=1G
+run_for_database_services "doctrine:fixtures:load --no-interaction --no-debug" -d memory_limit=1G
--- symfony/config/bundles.php
+++ symfony/config/bundles.php
@@ -13,7 +13,7 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
-    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['all' => true],
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
     PixelOpen\CloudflareTurnstileBundle\PixelOpenCloudflareTurnstileBundle::class => ['all' => true],
