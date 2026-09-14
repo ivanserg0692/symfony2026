4a58c67ddb6a7bc7535e94ecd8861d38903ac351
ivan <ivan.serg0692@gmail.com>
2026-09-14 14:33:48 +0800

    task 10: prevent containers from staying up when the web server stops

--- docker/php-symfony-cli/dev-web-server.sh
+++ docker/php-symfony-cli/dev-web-server.sh
@@ -4,5 +4,4 @@ set -euo pipefail
 export PATH="/root/.symfony5/bin:${PATH}"
 
 symfony server:stop || true
-symfony serve --allow-http --no-tls --listen-ip=0.0.0.0 --port=8000 --daemon
-exec tail -f /dev/null
+exec symfony serve --allow-http --no-tls --listen-ip=0.0.0.0 --port=8000
