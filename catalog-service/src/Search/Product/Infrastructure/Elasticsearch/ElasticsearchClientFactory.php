14e07284c1edae59a111fecaf345ef781fdd932d
ivan <ivan.serg0692@gmail.com>
2026-09-19 01:01:14 +0800

    task 10: add the persistent connection for ES

--- catalog-service/src/Search/Product/Infrastructure/Elasticsearch/ElasticsearchClientFactory.php
+++ catalog-service/src/Search/Product/Infrastructure/Elasticsearch/ElasticsearchClientFactory.php
@@ -4,6 +4,7 @@ namespace App\Search\Product\Infrastructure\Elasticsearch;
 
 use Elastic\Elasticsearch\Client;
 use Elastic\Elasticsearch\ClientBuilder;
+use Elastic\Transport\Client\Curl;
 
 final readonly class ElasticsearchClientFactory
 {
@@ -16,7 +17,19 @@ final readonly class ElasticsearchClientFactory
 
     public function create(): Client
     {
-        $builder = ClientBuilder::create()->setHosts([$this->elasticsearchUrl]);
+        $share = curl_share_init_persistent([
+            CURL_LOCK_DATA_CONNECT,
+            CURL_LOCK_DATA_DNS,
+        ]);
+
+        if ($share === false) {
+            throw new \RuntimeException('Unable to initialize the persistent Elasticsearch cURL share handle.');
+        }
+
+        $builder = ClientBuilder::create()
+            ->setHosts([$this->elasticsearchUrl])
+            ->setHttpClient(new Curl())
+            ->setHttpClientOptions([CURLOPT_SHARE => $share]);
 
         if ($this->elasticsearchUsername !== "") {
             $builder->setBasicAuthentication($this->elasticsearchUsername, $this->elasticsearchPassword);
