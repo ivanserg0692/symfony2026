43f91ae663bcd9ef7e2055877a13f78f61f2a082
ivan <ivan.serg0692@gmail.com>
2026-09-20 17:13:39 +0800

    task 10: add NGINX_WORKER_COUNT=auto

--- docker/php-symfony-cli/prod-web-server.sh
+++ docker/php-symfony-cli/prod-web-server.sh
@@ -12,22 +12,33 @@ require_positive_integer() {
 }
 
 require_positive_integer PHP_FPM_MAX_CHILDREN
-require_positive_integer NGINX_WORKER_PROCESSES
 
-fastcgi_keepalive_per_worker=$((PHP_FPM_MAX_CHILDREN / NGINX_WORKER_PROCESSES))
+if [[ "${NGINX_WORKER_COUNT:-}" == "auto" ]]; then
+    if ! nginx_worker_count="$(getconf _NPROCESSORS_ONLN 2>/dev/null)" \
+        || [[ ! "$nginx_worker_count" =~ ^[1-9][0-9]*$ ]]; then
+        printf '%s\n' 'Unable to determine the Nginx worker count with getconf _NPROCESSORS_ONLN' >&2
+        exit 1
+    fi
+else
+    require_positive_integer NGINX_WORKER_COUNT
+    nginx_worker_count="$NGINX_WORKER_COUNT"
+fi
 
-if ((fastcgi_keepalive_per_worker < 1)); then
-    printf '%s\n' 'NGINX_WORKER_PROCESSES must not exceed PHP_FPM_MAX_CHILDREN' >&2
-    exit 1
+fastcgi_keepalive_per_worker=$(((PHP_FPM_MAX_CHILDREN * 80) / (100 * nginx_worker_count)))
+
+if ((fastcgi_keepalive_per_worker > 0)); then
+    fastcgi_keepalive_directive="keepalive ${fastcgi_keepalive_per_worker};"
+else
+    fastcgi_keepalive_directive='# FastCGI keepalive is disabled because the calculated cache size is 0.'
 fi
 
 sed -E \
-    "s/^worker_processes[[:space:]]+[^;]+;/worker_processes ${NGINX_WORKER_PROCESSES};/" \
+    "s/^worker_processes[[:space:]]+[^;]+;/worker_processes ${nginx_worker_count};/" \
     /etc/nginx/nginx.conf.template \
     > /etc/nginx/nginx.conf
 
 sed \
-    "s/__FASTCGI_KEEPALIVE_PER_WORKER__/${fastcgi_keepalive_per_worker}/" \
+    "s/__FASTCGI_KEEPALIVE_DIRECTIVE__/${fastcgi_keepalive_directive}/" \
     /etc/nginx/sites-available/default.template \
     > /etc/nginx/sites-available/default
 
